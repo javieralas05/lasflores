135ed49da1877cdcd9ed19f26741f1334fb1c76d
javier <javier.alas05@gmail.com>
2021-07-01 08:59:58 -0600

    layout completed

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function iniciarMap(){
     var coord = {lat:13.172968715128054,lng: -88.11768682665053};
     var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 16,
+      zoom: 13,
       center: coord,
     });
     var marker = new google.maps.Marker({
